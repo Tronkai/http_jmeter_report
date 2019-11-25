12403f56a3ba7a90063e27587c2e027faadcba09
kaizhu@tron.network <kaizhu@tron.network>
2019-11-25 15:52:38 +0800

    change

--- start_tronscan.sh
+++ start_tronscan.sh
@@ -12,7 +12,8 @@ kill -9 $(cat pid)
 nohup java -jar /home/java-tron/tronscan_workspace/tronscan_spring/build/libs/http-1.0-SNAPSHOT.jar  >> /home/java-tron/tronscan_workspace/http.log 2>&1 &
 pid=$!
 echo $pid > pid
-nohup /home/java-tron/apache-jmeter-5.2/bin/jmeter -n -t /home/java-tron/tronscan_workspace/tron-test/src/test/resources/http_jmeter.jmx -l result.jtl -e -o /home/java-tron/http_jmeter_report/
+/home/java-tron/apache-jmeter-5.2/bin/jmeter -n -t /home/java-tron/tronscan_workspace/tron-test/src/test/resources/http_jmeter.jmx -l result.jtl -e -o /home/java-tron/http_jmeter_report/
+cd /home/java-tron/http_jmeter_report/
 git add .
 git commit -m "report"
 git push
